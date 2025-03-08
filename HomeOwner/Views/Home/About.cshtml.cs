7b95fde37f05df0cf34f6c365a00f09366786e89
Rodriguez1718 <krisjohn.rodriguez@gmail.com>
2025-03-09 04:08:19 +0800

    first commit

---
+++ HomeOwner/Views/Home/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeOwner.Views.Home
+{
+    public class about_usModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HomeOwner/wwwroot/js/nav-init.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('[data-classy-nav]').each(function () {
+        var $nav = $(this);
+        // Build options from data attributes
+        var options = {
+            theme: $nav.data('theme') || 'light',
+            breakpoint: $nav.data('breakpoint') || 991,
+            openCloseSpeed: $nav.data('open-close-speed') || 300,
+            alwaysHidden: $nav.data('always-hidden') === true,
+            openMobileMenu: $nav.data('open-mobile-menu') || 'left',
+            dropdownRtl: $nav.data('dropdown-rtl') === true,
+            stickyNav: $nav.data('sticky-nav') === true,
+            stickyFooterNav: $nav.data('sticky-footer-nav') === true
+        };
+
+        $nav.classyNav(options);
+    });
+});
