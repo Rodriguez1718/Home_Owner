9b4cdbf0d547a7c4e03e3a503e853b7de90a2c95
unknown <admin47@ccslab530u47.ccs.local>
2025-05-07 20:04:14 +0800

    designs are updated for admin

--- HomeOwner/obj/Debug/net8.0/HomeOwner.AssemblyInfo.cs
+++ HomeOwner/obj/Debug/net8.0/HomeOwner.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeOwner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5156e419c7d85277e894482a6fd589020f35a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016b7a6a563caf6ce3e0073f16472244fcdb55ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeOwner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeOwner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
